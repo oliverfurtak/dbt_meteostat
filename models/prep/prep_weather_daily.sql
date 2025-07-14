8bc9fb2293b8ac682e5a99df0a18c98cd25aa2af
Oliver Furtak <furtak@mailbox.org>
2025-07-14 10:12:56 +0000

    last try

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -15,10 +15,10 @@ add_features AS (
 add_more_features AS (
     SELECT *
 		, (CASE 
-			WHEN month_name in ('December, January, Feburary') THEN 'winter'
-			WHEN month_name in ('March, April, May') THEN 'spring'
-            WHEN month_name in ('June, July, August') THEN 'summer'
-            WHEN month_name in ('September, October, November') THEN 'autumn'
+			WHEN month_name in ('december', 'january', 'february') THEN 'winter'
+			WHEN month_name in ('march', 'april', 'may') THEN 'spring'
+            WHEN month_name in ('june', 'july', 'august') THEN 'summer'
+            WHEN month_name in ('september', 'october', 'november') THEN 'autumn'
 		END) AS season
     FROM add_features
 )
