8ba14b005aa5c696f0b57919d77e8709785aae87
Oliver Furtak <furtak@mailbox.org>
2025-07-14 10:53:10 +0000

    last?

---
+++ models/prep/prep_airports.sql
@@ -0,0 +1,14 @@
+WITH airports_reorder AS (
+    SELECT faa
+    	   , country
+           , region
+           , name
+           , lat
+           , lon
+           , alt
+           , tz
+           , dst
+           , city
+    FROM {{ref('staging_airports')}}
+)
+SELECT * FROM airports_reorder
